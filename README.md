26d4931446c966d4b0ff7d28ed9d27d60913da33
luis-sarro <lsb@dia.uned.es>
2023-11-14 15:43:12 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# CEURA
+Data Science
+
+Para cargar el entorno ceura:
+
+conda env create -f environment.yml
---
+++ check_setup_01.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import scipy as sc
+import statsmodels as st
+import seaborn as sb
+import pymc3 as pm
+
+print("numpy version ", np.__version__)
+print("pandas version ", pd.__version__)
+print("scipy version ", sc.__version__)
+print("statsmodels version ", st.__version__)
+print("seaborn version ", sb.__version__)
+print("pymc3 version ", pm.__version__)
